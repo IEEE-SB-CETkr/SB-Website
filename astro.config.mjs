49fb37ebe13f65c21788c439c77fc66c1da3c401
Anukheth Sunil M <anukhethsunil@gmail.com>
2025-12-20 23:00:50 +0530

    optimizations and fix spelling

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,34 +16,30 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import playformCompress from '@playform/compress';
-import mdx from '@astrojs/mdx';
+import playformCompress from "@playform/compress";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.GITHUB_PAGES
-    ? 'https://kethu-x86.github.io/ieeesbcetkr/'
-    : 'https://example.com',
-  base: process.env.GITHUB_PAGES ? '/ieeesbcetkr/' : '/',
-  integrations: [sitemap(),
-  react(),
-  playformCompress(
-    {
+  site: "https://ieeesbcetkr.org",
+  base: "/",
+  integrations: [
+    sitemap(),
+    react(),
+    playformCompress({
       Image: true,
-      CSS: true,
-      HTML: true
-    }
-  ),
-  mdx({
-    smartypants: false,
-  })],
+      CSS: false,
+      HTML: true,
+    }),
+    mdx({
+      smartypants: false,
+    }),
+  ],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 });
